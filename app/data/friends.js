b0b263c469ace2454070d786a3fab73e1b0af9a7
Julian <julian@Julians-MBP-3.home>
2019-05-12 17:34:46 -0400

    everything except the CSS

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friends = [
+	{
+		name: "Mickey",
+		photo: "https://i.pinimg.com/originals/65/d6/cf/65d6cfcb2bb5193d6d160157b34b2bd0.png",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+	},
+	{
+		name: "Ronald",
+		photo: "https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/815/cached.offlinehbpl.hbpl.co.uk/news/OMC/C785C365-DB1F-ABBE-BEF5F76B8E2868E1.jpg",
+		scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	},
+	{
+		name: "Mario",
+		photo: "https://supermariorun.com/assets/img/stage/mario03.png",
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+	{
+		name: "Fred",
+		photo: "https://www.standingstills.com/pub/media/catalog/product/cache/75eed2686e01eb22cb4050b2f40ddf97/c/a/cad104-1-1.jpg",
+		scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+	{
+		name: "Betty",
+		photo: "https://res.cloudinary.com/teepublic/image/private/s--jNDkK6EU--/t_Preview/b_rgb:c62b29,c_limit,f_jpg,h_630,q_90,w_630/v1510029388/production/designs/2030784_2.jpg",
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Velma",
+        photo: "https://vignette.wikia.nocookie.net/scoobydoo/images/9/9d/Velma_Dinkley.png/revision/latest?cb=20160213120532",
+        scores: [5, 1, 4, 2, 3, 3, 2, 4, 1, 5]
+    },
+    {
+        name: "Cruella",
+        photo: "https://s1.thcdn.com/productimg/1600/1600/11539263-1734511989949973.jpg",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    },
+    {
+        name: "Princess Peach",
+        photo: "https://aff5fa4925746bf9c161-fb36f18ca122a30f6899af8eef8fa39b.ssl.cf5.rackcdn.com/images/Masthead_Peach.17345b1513ac044897cfc243542899dce541e8dc.9afde10b.png",
+        scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+    }
+];
+
+module.exports = friends; 
